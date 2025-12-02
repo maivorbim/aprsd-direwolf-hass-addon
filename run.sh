ae1d1a200a8c037bec6fd974afdfe67b65d211fc
maivorbim <34894081+maivorbim@users.noreply.github.com>
2025-12-02 22:41:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 set -e
-...
+
+if [ ! -f /data/aprsd.yml ]; then
+    echo "Missing /data/aprsd.yml"
+    sleep infinity
+fi
+
+if [ ! -f /data/direwolf.conf ]; then
+    echo "Missing /data/direwolf.conf"
+    sleep infinity
+fi
+
+/start-direwolf.sh &
+
+sleep 3
+
+/start-aprsd.sh &
+
+python3 /mqtt_bridge.py &
+
+wait -n
